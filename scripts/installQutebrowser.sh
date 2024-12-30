eb04b969a58f8b93563836c706460c8a3d730f99
isCordial <21206554+isCordial@users.noreply.github.com>
2024-12-30 15:05:44 -0800

    set permissisions for qutebrowser executable during install

--- scripts/installQutebrowser.sh
+++ scripts/installQutebrowser.sh
@@ -1,4 +1,5 @@
 cp ./bin/qutebrowser $HOME/.local/bin/qutebrowser # add to local $PATH
+sudo chmod +x $HOME/.local/bin/qutebrowser
 cp -r ../.config/qutebrowser/ $HOME/.config/ # install .config dotfiles
 
 git clone https://github.com/qutebrowser/qutebrowser.git $HOME/repos/qutebrowser
@@ -8,3 +9,5 @@ cd $HOME/repos/qutebrowser
 # apt install --no-install-recommends git ca-certificates python3 python3-venv libgl1 libxkbcommon-x11-0 libegl1 libfontconfig1 libglib2.0-0 libdbus-1-3 libxcb-cursor0 libxcb-icccm4 libxcb-keysyms1 libxcb-shape0 libnss3 libxcomposite1 libxdamage1 libxrender1 libxrandr2 libxtst6 libxi6 libasound2
 
 python3 scripts/mkvenv.py
+
+echo 'Qutebrowser installer finished.'
