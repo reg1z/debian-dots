64fb40974741602221f3823749cec20042afbb5b
reg1z <21206554+reg1z@users.noreply.github.com>
2025-06-29 13:30:31 -0700

    i3status

--- scripts/i3copy.sh
+++ scripts/i3copy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cp -rfv ../.config/i3 $HOME/.config
+mkdir ~/.config/i3
+mkdir ~/.config/i3status
+
+cp -rfv ../.config/i3/* ~/.config/i3
+cp -rfv ../.config/i3status/* ~/.config/i3status
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,16 +2,24 @@
 
 sudo apt update
 sudo apt install i3 autotiling rofi neovim tmux -y
+sudo apt full-upgrade -y
+
+# echo 'Would you like to full-upgrade all available packages?: '
+# read input
+#
+# if [ $input = 'yes' ] || [ $input = 'y' ]; then
+#   sudo apt full-upgrade -y
+# fi
 
 # directory setup
 mkdir -p $HOME/.config
 
 # shell(s) / aliases
-echo "alias v='nvim'" >> $HOME/.bashrc
-echo "alias ls='ls -a'" >> $HOME/.bashrc
+echo "alias v='nvim'" >>$HOME/.bashrc
+echo "alias ls='ls -a'" >>$HOME/.bashrc
 
-echo "alias v='nvim'" >> $HOME/.zshrc
-echo "alias ls='ls -a'" >> $HOME/.zshrc
+echo "alias v='nvim'" >>$HOME/.zshrc
+echo "alias ls='ls -a'" >>$HOME/.zshrc
 
 # home
 cp -fv ../vim/.vimrc $HOME/.vimrc
@@ -19,16 +27,14 @@ cp -rfv ../tmux/.tmux $HOME
 cp -fv ../tmux/.tmux.conf $HOME/.tmux.conf
 
 # .config
+mkdir ~/.config/i3
+mkdir ~/.config/i3status
+mkdir ~/.config/nvim
+
 cp -rfv ../.config/i3 $HOME/.config
+cp -rfv ../.config/i3status/* ~/.config/i3status
 cp -rfv ../.config/rofi $HOME/.config
 cp -rfv ../.config/nvim $HOME/.config
-
-echo 'Would you like to apt full-upgrade all available packages?: '
-read input
-
-if [ $input = 'yes' ] || [ $input = 'y' ]; then
-  sudo apt full-upgrade -y
-fi
 
 echo 'Would you like to install QuteBrowser (via github) ?'
 read input
@@ -36,6 +42,5 @@ read input
 if [ $input = 'yes' ] || [ $input = 'y' ]; then
   ./installQutebrowser.sh
 fi
-
 
 echo 'Fin.'
